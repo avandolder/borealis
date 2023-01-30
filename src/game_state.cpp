9bd16dc447a4ee65ad4ce1061d0cc10921aefbe2
Adam Vandolder <adam.vandolder@gmail.com>
2023-01-29 21:32:29 -0500

    Add camera offset region

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -13,19 +13,39 @@ struct Pos final : rl::Vector2 {
   explicit Pos(rl::Vector2 v) : rl::Vector2(v) {}
   Pos(float x, float y) : rl::Vector2(x, y) {}
 };
+
 struct Vel final : rl::Vector2 {
   explicit Vel(rl::Vector2 v) : rl::Vector2(v) {}
   Vel(float x, float y) : rl::Vector2(x, y) {}
 };
 
-struct Player final {};
+struct Shape final {
+  int w = 0;
+  int h = 0;
+  rl::Color color;
+};
+
+struct Player final {
+  rl::Vector2 offset;
+  static constexpr const float max_offset = 50;
+};
+
+static auto clamp(const rl::Vector2& v,
+                  const rl::Vector2& lo,
+                  const rl::Vector2& hi) -> rl::Vector2 {
+  return {std::clamp(v.x, lo.x, hi.x), std::clamp(v.y, lo.y, hi.y)};
+}
 
 GameState::GameState(rl::Window& window, TileMap& tmap)
-    : camera_(window.GetSize() / 2.f, rl::Vector2::Zero(), 0.0f, 2.f),
+    : camera_(window.GetSize() / 2.f, rl::Vector2::Zero(), 0.0f, 3.f),
       tmap_(tmap),
       player_(world_.create()) {
   world_.emplace<Pos>(player_, tmap.size() / 2.f);
+  camera_.target = tmap.size() / 2.f;
+
   world_.emplace<Vel>(player_, rl::Vector2::Zero());
+  world_.emplace<Shape>(player_,
+                        Shape{.w = 10, .h = 10, .color = rl::BLACK});
   world_.emplace<Player>(player_);
 }
 
@@ -47,20 +67,31 @@ void GameState::update(GameData game) {
                   : IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S) ? 1.f
                                                             : 0.f}
           .Normalize();
-  world_.get<Vel>(player_) = Vel(delta * 200);
+  auto& vel = world_.get<Vel>(player_);
+  vel = Vel(delta * 200 * dt);
 
   const auto tmap_size = tmap_.size();
-  world_.view<Pos, Vel>().each([&](auto& pos, auto& vel) {
-    pos.x = std::clamp(0.f, pos.x + vel.x * dt, tmap_size.x);
-    pos.y = std::clamp(0.f, pos.y + vel.y * dt, tmap_size.y);
-  });
+  auto& pos = world_.get<Pos>(player_);
+  pos = Pos(clamp(rl::Vector2::Zero(), pos + vel, tmap_size));
 
-  const auto& pos = world_.get<Pos>(player_);
-  camera_.target = rl::Vector2(
-      std::clamp(pos.x, camera_.offset.x / camera_.zoom,
-                 tmap_size.x - camera_.offset.x / camera_.zoom),
-      std::clamp(pos.y, camera_.offset.y / camera_.zoom,
-                 tmap_size.y - camera_.offset.y / camera_.zoom));
+  auto& [offset] = world_.get<Player>(player_);
+  if (delta == rl::Vector2::Zero()) {
+    // When not moving, tween the camera towards the player.
+    auto progress = offset.Length() / Player::max_offset;
+    offset = offset.LengthSqr() < 1
+                 ? rl::Vector2{0, 0}
+                 : rl::Vector2{offset * (1 - progress * dt * 5)};
+  } else {
+    auto old_offset{offset};
+    offset -= vel;
+    if (offset.Length() > Player::max_offset) {
+      offset = old_offset;
+    }
+  }
+
+  camera_.target =
+      clamp(pos + offset, rl::Vector2{camera_.offset} / camera_.zoom,
+            tmap_size - rl::Vector2{camera_.offset} / camera_.zoom);
 }
 
 void GameState::draw() {
@@ -68,9 +99,16 @@ void GameState::draw() {
 
   tmap_.draw(camera_);
 
-  world_.view<Pos>().each([](const auto& pos) {
-    DrawRectangle(pos.x, pos.y, 10, 10, rl::WHITE);
-  });
+  world_.view<Pos, Shape>().each(
+      [](const auto& pos, const auto& shape) {
+        DrawRectangle(pos.x - shape.w / 2, pos.y - shape.h / 2, shape.w,
+                      shape.h, shape.color);
+      });
+
+#ifdef DEBUG
+  DrawCircleLines(camera_.target.x, camera_.target.y,
+                  Player::max_offset, rl::Color::Magenta());
+#endif
 
   camera_.EndMode();
 }
